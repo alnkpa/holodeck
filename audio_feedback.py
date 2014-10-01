cf964ed7ca80d9523c65d858597e94dc3cf36094
Xman <xman@wantoto.com>
2014-10-01 16:16:33 +0200

    add all the voices from quantumball

--- audio_feedback.py
+++ audio_feedback.py
@@ -5,9 +5,13 @@ import socket
 
 pygame.mixer.init()
 
+bump = pygame.mixer.Sound(u"Bump.wav")
+ding = pygame.mixer.Sound(u"Ding.wav")
 noise = pygame.mixer.Sound(u"Buzz.wav")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 29999))
+
 
 while True:
     data = sock.recv(1024)
@@ -17,6 +21,13 @@ while True:
     elif data.startswith("START"):
         print "foo"
         noise.play(loops=-1)
+    elif data.startswith("Bump"):
+        number = data[-1:]
+        pygame.mixer.Sound(number+".wav").play()
+        bump.play()
+    elif data.startswith("Move"):
+        number = data[-1:]
+        pygame.mixer.Sound(number+".wav").play()
     elif data.startswith("END"):
         break
 
