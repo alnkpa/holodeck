564baff3dee88dea387f414dd1114a2b49c51ecd
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2015-04-01 17:13:26 +0200

    New sounds. More sounds. 1a, move!

--- audio_feedback.py
+++ audio_feedback.py
@@ -9,25 +9,34 @@ bump = pygame.mixer.Sound(u"Bump.wav")
 ding = pygame.mixer.Sound(u"Ding.wav")
 noise = pygame.mixer.Sound(u"Buzz.wav")
 
+numbers = map(str, range(9)) + ["U"]
+letters = ['', 'a', 'b', 'c', 'd', 'e', 'f']
+
+numbersounds = {k: pygame.mixer.Sound(k + ".wav") for k in (n+l for n in numbers for l in letters)}
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 29999))
 
 
 while True:
-    data = sock.recv(1024)
-    print data
-    if data.startswith("STOP"):
-        noise.stop()
-    elif data.startswith("START"):
-        print "foo"
-        noise.play(loops=-1)
-    elif data.startswith("Bump"):
-        number = data[-1:]
-        pygame.mixer.Sound(number+".wav").play()
-        bump.play()
-    elif data.startswith("Move"):
-        number = data[-1:]
-        pygame.mixer.Sound(number+".wav").play()
-    elif data.startswith("END"):
-        break
+	try:
+		data = sock.recv(1024)
+		print data
+		if data.startswith("STOP"):
+			noise.stop()
+		elif data.startswith("START"):
+			noise.play(loops=-1)
+		elif data.startswith("Bump"):
+			number = data.partition("Bump")[2].strip()
+			numbersounds[number].play()
+			bump.play()
+		elif data.startswith("Move"):
+			number = data.partition("Move")[2].strip()
+			numbersounds[number].play()
+		elif data.startswith("END"):
+			break
+	except KeyboardInterrupt:
+		break
+	except:
+		pass
 
