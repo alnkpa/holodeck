a27445146e4f9ea3a268d19e76980d4cb4eaebd9
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2015-04-09 16:37:01 +0200

    Busy wait and careless abuse of system's socket buffer queue

--- audio_feedback.py
+++ audio_feedback.py
@@ -4,10 +4,12 @@ import pygame.mixer
 import socket
 import os
 
+pygame.mixer.init()
+
 soundName = lambda s: os.path.join("data", s) + ".wav"
 sound = lambda s: pygame.mixer.Sound(soundName(s))
 
-pygame.mixer.init()
+load = lambda s: pygame.mixer.music.load
 
 bump = sound("Bump")
 ding = sound("Ding")
@@ -21,30 +23,35 @@ letters = [''] + list("abcdefgh")
 
 numbersounds = {k: sound(k) for k in (n+l for n in numbers for l in letters)}
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", 29999))
-
-
-while True:
-	try:
-		data = sock.recv(1024)
-		print data
-		if data.startswith("STOP"):
-			noise.stop()
-		elif data.startswith("START"):
-			noise.play(loops=-1)
-		elif data.startswith("Bump"):
-			number = data.partition("Bump")[2].strip().lower()
-			numbersounds[number].play()
-			bump.play()
-		elif data.startswith("Move"):
-			number = data.partition("Move")[2].strip().lower()
-			print number
-			numbersounds[number].play()
-		elif data.startswith("END"):
-			break
-	except KeyboardInterrupt:
-		break
-	except:
+channel = None
+def play(command, data):
+	global channel
+	number = data.partition(command)[2].strip().lower()
+	while channel is not None and channel.get_busy():
 		pass
+	channel = numbersounds[number].play()
+
+if __name__ == "__main__":
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(("", 29999))
+
+	while True:
+		try:
+			data = sock.recv(1024)
+			print data
+			if data.startswith("STOP"):
+				noise.stop()
+			elif data.startswith("START"):
+				noise.play(loops=-1)
+			elif data.startswith("Bump"):
+				play("Bump", data)
+				bump.play()
+			elif data.startswith("Move"):
+				play("Move", data)
+			elif data.startswith("END"):
+				break
+		except KeyboardInterrupt:
+			break
+		except:
+			pass
 
