16f74c1e1ee1adabdc21cf0015a2d0d7a03d1cf1
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2015-04-09 15:29:10 +0200

    Moved files

--- audio_feedback.py
+++ audio_feedback.py
@@ -2,17 +2,24 @@
 
 import pygame.mixer
 import socket
+import os
+
+soundName = lambda s: os.path.join("data", s) + ".wav")
+sound = lambda s: pygame.mixer.Sound(soundName(s))
 
 pygame.mixer.init()
 
-bump = pygame.mixer.Sound(u"Bump.wav")
-ding = pygame.mixer.Sound(u"Ding.wav")
-noise = pygame.mixer.Sound(u"Buzz.wav")
+bump = sound("Bump")
+ding = sound("Ding")
+noise = sound("Buzz")
 
+# Sounds generated with 
+#   say -r 0.7 '0--A' -o 0a.aiff
+#   ffmpeg -i 0a.aiff 0a.wav
 numbers = map(str, range(9)) + ["U"]
 letters = ['', 'a', 'b', 'c', 'd', 'e', 'f']
 
-numbersounds = {k: pygame.mixer.Sound(k + ".wav") for k in (n+l for n in numbers for l in letters)}
+numbersounds = {k: sound(k) for k in (n+l for n in numbers for l in letters)}
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 29999))
@@ -27,11 +34,11 @@ while True:
 		elif data.startswith("START"):
 			noise.play(loops=-1)
 		elif data.startswith("Bump"):
-			number = data.partition("Bump")[2].strip()
+			number = data.partition("Bump")[2].strip().lower()
 			numbersounds[number].play()
 			bump.play()
 		elif data.startswith("Move"):
-			number = data.partition("Move")[2].strip()
+			number = data.partition("Move")[2].strip().lower()
 			numbersounds[number].play()
 		elif data.startswith("END"):
 			break
